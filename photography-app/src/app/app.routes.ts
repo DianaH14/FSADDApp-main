91efd090075b339cb48c86552edaa12e0930948f
Diana Halmageanu <diana.halmageanu@cpp.canon>
2026-03-06 10:03:38 +0200

    add backend and mock an enpoint for swagger

--- photography-app/src/app/app.routes.ts
+++ photography-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/gallery', pathMatch: 'full' },
+  { path: 'gallery', component: GalleryComponent }
+];
